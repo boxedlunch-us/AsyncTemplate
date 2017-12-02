d524e86fb24cca6e494bdfcc378019836d4de866
Ricky Nelson <tya0411@gmail.com>
2017-12-02 16:30:55 -0600

    Initial commit

---
+++ AsyncTemplate.ps1
@@ -0,0 +1,40 @@
+﻿#get current script directory 
+$ScriptDir = Split-Path $script:MyInvocation.mycommand.path
+
+# JSON data; can also be a URL
+$json = "\\servername\http_host\data\server_data.json"
+
+# Retrieve daily server list content
+$serverInfo = (Get-Content $json) -join "`n" | ConvertFrom-Json
+
+#convert into searchable object
+$serverInfo = $serverInfo.data
+
+
+
+foreach ($server in $serverInfo) {
+    $running = @(Get-Job | Where-Object { $_.State -eq 'Running' })
+    if ($running.Count -ge 4) {
+        $running | Wait-Job -Any | Out-Null
+    }
+
+    Write-Host "Starting job for " $server.hostname
+    Start-Job {
+        # dew something
+        # then return it
+        
+    } | Out-Null
+}
+
+# Wait for all jobs to complete and results ready to be received
+Wait-Job * | Out-Null
+
+# Process the results
+foreach($job in Get-Job)
+{
+    
+    $result = Receive-Job $job
+    Write-Host $result
+}
+
+Remove-Job -State Completed
